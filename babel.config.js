aca2a0db9e6ee75dc44793903b341a92dfb63891
wangzhendong <249558764@qq.com>
2020-11-12 16:52:11 +0800

    初始化

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,15 @@
 module.exports = {
-  presets: [
-    '@vue/cli-plugin-babel/preset'
-  ]
-}
+  presets: ["@vue/cli-plugin-babel/preset"],
+  // 自动按需引入组件 (推荐)
+  plugins: [
+    [
+      "import",
+      {
+        libraryName: "vant",
+        libraryDirectory: "es",
+        style: true,
+      },
+      "vant",
+    ],
+  ],
+};
